29a235a9a06887d1aa7ad98920bc13dfe4f2cfab
Lavie-3107 <nguyenanhtu2204tb@gmail.com>
2023-02-22 16:25:50 +0700

    update package json

--- src/Component/Menu_right/Header/Avatar/Avatar.js
+++ src/Component/Menu_right/Header/Avatar/Avatar.js
@@ -1,5 +1,5 @@
 export default function Avatar(){
     return(
-        <img src='https://i.pinimg.com/originals/89/54/38/895438247efa788551d1919d44f176ca.png' alt="#"></img>
+        <img src='https://khoinguonsangtao.vn/wp-content/uploads/2022/07/avatar-gau-cute.jpg' alt="#"></img>
     )
 }
